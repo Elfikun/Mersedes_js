0f2252e5e5ed829cfc92213df9e92483847b5910
Elfikun <28907262+Elfikun@users.noreply.github.com>
2021-09-23 16:31:43 +0300

    scroll

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,7 +1,16 @@
 const linksHead = document.querySelectorAll('.menu-list__link');
+const mainScroll = document.querySelector('.main__scroll');
+const newArray = [...linksHead, mainScroll];
 
-linksHead.forEach(link => {
+newArray.forEach(link => {
     link.addEventListener('click', () => {
-        
-    })
-})
+        event.preventDefault();
+
+        const ID = event.target.getAttribute('href').substr(1);
+
+        document.getElementById(ID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
